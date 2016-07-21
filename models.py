2a3b8e3ab2a0ebe40026e88c19c52825ac83a64c
lamarmeigs <lmeigs@shiftgig.com>
2016-07-20 19:01:17 -0500

    Refactor models for easier expansion
    Separate on-save cleaning behavior (in common base class) from locating
    cleaner methods (in inheriting classes). Also, ensure all models are
    abstract.

--- models.py
+++ models.py
@@ -1,24 +1,64 @@
 from django.db.models import Model
 
-# TODO: field cleaners should be definable anywhere, and not have any
-# restrictions on naming. Perhaps create an external registry?
 
-
-class CleanFieldsModel(Model):
+class BaseCleanFieldsModel(Model):
     """Represents a base model that calls registered field cleaners on save.
 
-    This class enables the use of special field cleaning methods which will be
-    run every time `save` is called. These methods should accept the current
-    value of the field, return a cleaned value (or raise an error), and adhere
-    to the name convention `clean_{field_name}`.
+    This abstract base model provides the ability to invoke methods that clean
+    field values on save. Such methods should accept the current value of the
+    field and return a finalized value to commit to the database (or raise an
+    error).
+
+    This class should not be inherited directly. It does not provide a
+    strategy to locate the cleaner methods; this feature is provided by
+    child classes that implement the `_get_field_cleaner` method.
     """
+    class Meta:
+        abstract = True
 
     def save(self, *args, **kwargs):
-        """Call registered field cleaner methods before saving."""
+        """Call cleaners for each field before saving."""
         field_names = [field.name for field in self._meta.get_fields()]
         for field_name in field_names:
-            field_cleaner = getattr(self, 'clean_{}'.format(field_name), None)
-            if field_cleaner and callable(field_cleaner):
+            field_cleaner = self._get_field_cleaner(field_name)
+            if field_cleaner:
                 field_value = getattr(self, field_name)
                 setattr(self, field_name, field_cleaner(field_value))
-        return super(CleanFieldsModel, self).save(*args, **kwargs)
+        return super(BaseCleanFieldsModel, self).save(*args, **kwargs)
+
+    def _get_field_cleaner(self, field_name):
+        """Locate field cleaner callables for field with the given name.
+
+        Args:
+            field_name (str): name of the field on this model whose cleaners
+                to retrieve.
+
+        Return:
+            callable or None
+        """
+        raise NotImplementedError()
+
+
+class NaiveCleanFieldsModel(BaseCleanFieldsModel):
+    """An abstract model to support the use of specially-named cleaner methods.
+
+    This class locates cleaner methods by name, expecting them to exist on the
+    same model and adhere to the naming convention `clean_{field_name}`.
+    """
+    class Meta:
+        abstract = True
+
+    def _get_field_cleaner(self, field_name):
+        """Return any cleaners for the named field.
+
+        Args:
+            field_name (str): name of the field on this model whose cleaners
+                to retrieve.
+
+        Return:
+            callable or None
+        """
+        field_cleaner = getattr(self, 'clean_{}'.format(field_name), None)
+        if field_cleaner and not callable(field_cleaner):
+            field_cleaner = None
+        return field_cleaner
--- tests/test_models.py
+++ tests/test_models.py
@@ -3,35 +3,64 @@ from unittest import TestCase
 from django.db import models
 from mock import patch
 
-from clean_fields.models import CleanFieldsModel
+from clean_fields.models import BaseCleanFieldsModel, NaiveCleanFieldsModel
 
 
-class CleanFieldsModelTestCase(TestCase):
+class BaseCleanFieldsModelTestCase(TestCase):
+    def test_get_field_cleaner_not_implemented(self):
+        with self.assertRaises(NotImplementedError):
+            BaseCleanFieldsModel()._get_field_cleaner('field_name')
+
     @patch('django.db.models.Model.save')
     def test_save_without_cleaner(self, mock_save):
-        class CleanerlessModel(CleanFieldsModel):
+        class CleanerlessBaseModel(BaseCleanFieldsModel):
             some_field = models.IntegerField()
 
-        dummy = CleanerlessModel(some_field=5)
-        dummy.save()
-
-    @patch('django.db.models.Model.save')
-    def test_save_with_uncallable_cleaner(self, mock_save):
-        class UncallableCleanerModel(CleanFieldsModel):
-            some_field = models.IntegerField()
-            clean_some_field = 'not callable'
-
-        dummy = UncallableCleanerModel(some_field=5)
-        dummy.save()
+        dummy = CleanerlessBaseModel(some_field=5)
+        with patch.object(dummy, '_get_field_cleaner', return_value=None):
+            dummy.save()
+        self.assertEqual(dummy.some_field, 5)
 
     @patch('django.db.models.Model.save')
     def test_save_with_cleaner(self, mock_save):
-        class CleaningModel(CleanFieldsModel):
+        class CleaningBaseModel(BaseCleanFieldsModel):
+            some_field = models.IntegerField()
+
+        dummy = CleaningBaseModel(some_field=5)
+        with patch.object(
+            dummy,
+            '_get_field_cleaner',
+            return_value=lambda x: 42
+        ):
+            dummy.save()
+        self.assertEqual(dummy.some_field, 42)
+
+
+class NaiveCleanFieldsModelTestCase(TestCase):
+    def test_get_find_cleaner_with_missing_cleaner(self):
+        class CleanerlessNaiveModel(NaiveCleanFieldsModel):
+            some_field = models.IntegerField()
+
+        dummy = CleanerlessNaiveModel(some_field=5)
+        self.assertIsNone(dummy._get_field_cleaner('some_field'))
+
+    def test_get_find_cleaner_with_uncallable_cleaner(self):
+        class UncallableCleanerNaiveModel(NaiveCleanFieldsModel):
+            some_field = models.IntegerField()
+            clean_some_field = 'not callable'
+
+        dummy = UncallableCleanerNaiveModel(some_field=5)
+        self.assertIsNone(dummy._get_field_cleaner('some_field'))
+
+    def test_get_find_cleaner_returns_cleaner(self):
+        class CleaningNaiveModel(NaiveCleanFieldsModel):
             some_field = models.IntegerField()
 
             def clean_some_field(self, some_field):
                 return 42
 
-        dummy = CleaningModel(some_field=5)
-        dummy.save()
-        self.assertEqual(dummy.some_field, 42)
+        dummy = CleaningNaiveModel(some_field=5)
+        self.assertEqual(
+            dummy._get_field_cleaner('some_field'),
+            dummy.clean_some_field
+        )
