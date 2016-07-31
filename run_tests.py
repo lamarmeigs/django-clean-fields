348c2e08eb0449d2316bc9bcc7ef56f321939399
lamarmeigs <lmeigs@shiftgig.com>
2016-07-30 19:30:26 -0500

    Handle calling run_tests with no input

--- run_tests.py
+++ run_tests.py
@@ -7,7 +7,8 @@ from django.conf import settings
 from django.test.utils import get_runner
 
 
-def runtests(test_labels):
+def run_tests(test_labels=None):
+    test_labels = test_labels or ['tests']
     settings.configure(INSTALLED_APPS=['tests'])
     django.setup()
     TestRunner = get_runner(settings)
@@ -20,4 +21,4 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('test_labels', nargs='*', default=['tests'])
     args = parser.parse_args()
-    runtests(test_labels=args.test_labels)
+    run_tests(test_labels=args.test_labels)
