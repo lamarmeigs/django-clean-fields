622ed91fdd206a4ec04307348e0968f17311f5de
lamarmeigs <lmeigs@shiftgig.com>
2016-08-16 14:54:38 -0500

    Build cleans_field_with_context decorator
    Additional decorator invokes cleaner methods with a dictionary
    containing the model instance's field values.

--- clean_fields/decorators.py
+++ clean_fields/decorators.py
@@ -7,7 +7,9 @@ from django.db.models.signals import pre_save
 from django.dispatch import receiver
 
 from clean_fields.exc import CleanFieldsConfigurationError
-from clean_fields.utils import get_model_field_value, parse_field_ref
+from clean_fields.utils import (
+    get_model_field_names, get_model_field_value, parse_field_ref
+)
 
 
 def cleans_field(field_ref):
@@ -17,7 +19,7 @@ def cleans_field(field_ref):
         field_ref (str): a label for the model field to clean, following the
             convention `app_name.ModelName.field_name`
     """
-    field_name, model_label = parse_field_ref(field_ref)
+    model_label, field_name = parse_field_ref(field_ref)
 
     def _clean_wrapper(cleaner_function):
         # Register a pre-save signal handler that calls the cleaner_function
@@ -64,3 +66,60 @@ def cleans_field(field_ref):
 
         return _run_cleaner
     return _clean_wrapper
+
+
+def cleans_field_with_context(field_ref):
+    """Decorator to register field cleaning methods that require additional
+    field values as parameters on the pre_save signal.
+
+    Args:
+        field_ref (str): a label for the model field to clean, following the
+            convention `app_name.ModelName.field_name`
+    """
+    model_label, field_name = parse_field_ref(field_ref)
+
+    def _clean_with_context_wrapper(cleaner_function):
+        # Register a pre-save signal handler that calls the cleaner_function
+        # on a model instance, and assigns the result to the instance's field.
+        @receiver(pre_save, sender=model_label, weak=False)
+        def signal_handler(sender, instance, **kwargs):
+            # Collect all the model instance's field values in a dictionary
+            context = {}
+            for name in get_model_field_names(instance):
+                context[name] = get_model_field_value(instance, name)
+            try:
+                field_value = context[field_name]
+            except KeyError:
+                raise CleanFieldsConfigurationError(
+                    model_label,
+                    field_name,
+                    cleaner_function.__name__
+                )
+
+            # Invoke the cleaner_function callable in the most correct manner.
+            field_cleaner = getattr(instance, cleaner_function.__name__, None)
+            if field_cleaner is not None:
+                cleaned_value = field_cleaner(field_value, context)
+            else:
+                try:
+                    cleaned_value = cleaner_function(
+                        instance,
+                        field_value,
+                        context
+                    )
+                except TypeError as e:
+                    if (
+                        'takes exactly 2 arguments' not in str(e) and
+                        'takes 2 positional arguments' not in str(e)
+                    ):
+                        raise e
+                    cleaned_value = cleaner_function(field_value, context)
+            setattr(instance, field_name, cleaned_value)
+
+        # Define an additional wrapper to execute cleaner_function with
+        # given arguments. This ensures the wrapped method can still be called.
+        def _run_cleaner_with_context(*args, **kwargs):
+            return cleaner_function(*args, **kwargs)
+
+        return _run_cleaner_with_context
+    return _clean_with_context_wrapper
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -9,7 +9,7 @@ from django.db import models
 from django.db.models.signals import pre_save
 from mock import Mock, patch
 
-from clean_fields.decorators import cleans_field
+from clean_fields.decorators import cleans_field, cleans_field_with_context
 from clean_fields.exc import CleanFieldsConfigurationError
 
 
@@ -57,6 +57,53 @@ class CleansFieldTestCase(TestCase):
         """Ensures decorated callables can still be invoked independently"""
         cleaner = Mock()
         wrapped_cleaner = cleans_field('app.Model.field_name')(cleaner)
+        wrapped_cleaner(1, 2, 'foobar')
+        cleaner.assert_called_once_with(1, 2, 'foobar')
+
+
+class CleansFieldWithContextTestCase(TestCase):
+    """Tests the functionality of the cleans_field_with_context decorator"""
+
+    def test_pre_save_signal_handler_registered(self):
+        with patch.object(pre_save, 'connect') as mock_connect:
+            cleans_field_with_context('app.ModelName.field_name')(lambda x: x)
+        self.assertEqual(mock_connect.call_count, 1)
+
+    def test_signal_handler_raises_error_on_incorrect_field(self):
+        class BadFieldContextModel(models.Model):
+            some_field = models.IntegerField()
+
+            @cleans_field_with_context('tests.BadFieldContextModel.no_field')
+            def clean_some_field(self, some_field, data):
+                return some_field + 1
+
+        dummy = BadFieldContextModel(some_field=5)
+        with self.assertRaises(CleanFieldsConfigurationError) as ctx:
+            pre_save.send(dummy.__class__, instance=dummy)
+        self.assertIn('tests.BadFieldContextModel', str(ctx.exception))
+        self.assertIn('no_field', str(ctx.exception))
+        self.assertIn('clean_some_field', str(ctx.exception))
+
+    def test_signal_handler_raises_no_error_on_empty_field(self):
+        class NullableFieldContextModel(models.Model):
+            field = models.IntegerField(null=True, blank=True)
+
+            @cleans_field_with_context('tests.NullableFieldContextModel.field')
+            def clean_field(self, field, data):
+                return field + 1 if field else 1
+
+        dummy = NullableFieldContextModel(field=None)
+        try:
+            pre_save.send(dummy.__class__, instance=dummy)
+        except Exception as error:
+            self.fail(
+                'Sending pre_save signal resulted in unexpected error:\n\t'
+                '{error}'.format(error=str(error))
+            )
+
+    def test_returns_cleaner_executor(self):
+        cleaner = Mock()
+        wrapped_cleaner = cleans_field_with_context('app.Model.field')(cleaner)
         wrapped_cleaner(1, 2, 'foobar')
         cleaner.assert_called_once_with(1, 2, 'foobar')
 
@@ -147,6 +194,97 @@ class CleansFieldUseCasesTestCase(TestCase):
                 raise TypeError('some_field is the wrong type')
 
         dummy = ErrorRaisingCleanerModel(some_field=5, other_field='str')
+        with self.assertRaises(TypeError) as ctx:
+            pre_save.send(dummy.__class__, instance=dummy)
+        self.assertEqual(str(ctx.exception), 'some_field is the wrong type')
+
+
+class CleansFieldWithContextUseCasesTestCase(TestCase):
+    """Runs tests on possible use cases"""
+
+    def test_multiple_cleaners_for_single_field(self):
+        class ManyCleanersContextModel(models.Model):
+            field = models.IntegerField()
+
+            @cleans_field_with_context('tests.ManyCleanersContextModel.field')
+            def add_one(self, some_field, data):
+                return some_field + 1
+
+            @cleans_field_with_context('tests.ManyCleanersContextModel.field')
+            def add_two(self, some_field, data):
+                return some_field + 2
+
+        dummy = ManyCleanersContextModel(field=5)
+        pre_save.send(dummy.__class__, instance=dummy)
+        self.assertEqual(dummy.field, 8)
+
+    def test_single_cleaner_for_multiple_fields(self):
+        class MultipleFieldContextModel(models.Model):
+            field = models.IntegerField()
+            other = models.IntegerField()
+
+            @cleans_field_with_context('tests.MultipleFieldContextModel.field')
+            @cleans_field_with_context('tests.MultipleFieldContextModel.other')
+            def clean_integer_field(self, value, data):
+                return value + 1
+
+        dummy = MultipleFieldContextModel(field=4, other=5)
+        pre_save.send(dummy.__class__, instance=dummy)
+        self.assertEqual(dummy.field, 5)
+        self.assertEqual(dummy.other, 6)
+
+    def test_staticmethod_cleaner(self):
+        class StaticCleanerContextModel(models.Model):
+            field = models.IntegerField()
+
+            @staticmethod
+            @cleans_field_with_context('tests.StaticCleanerContextModel.field')
+            def clean_field(field, data):
+                return field + 1
+
+        dummy = StaticCleanerContextModel(field=5)
+        pre_save.send(dummy.__class__, instance=dummy)
+        self.assertEqual(dummy.field, 6)
+
+    def test_classmethod_cleaner(self):
+        class ClassCleanerContextModel(models.Model):
+            field = models.IntegerField()
+
+            @classmethod
+            @cleans_field_with_context('tests.ClassCleanerContextModel.field')
+            def clean_some_field(cls, some_field, data):
+                assert cls == ClassCleanerContextModel
+                return some_field + 1
+
+        dummy = ClassCleanerContextModel(field=5)
+        pre_save.send(dummy.__class__, instance=dummy)
+        self.assertEqual(dummy.field, 6)
+
+    def test_independent_function_cleaner(self):
+        class NoCleanerContextModel(models.Model):
+            some_field = models.IntegerField()
+
+        @cleans_field_with_context('tests.NoCleanerContextModel.some_field')
+        def clean_some_field(some_field, data):
+            return some_field + 1
+
+        dummy = NoCleanerContextModel(some_field=5)
+        pre_save.send(dummy.__class__, instance=dummy)
+        self.assertEqual(dummy.some_field, 6)
+
+    def test_cleaner_raises_type_error(self):
+        class ErrorContextModel(models.Model):
+            some_field = models.IntegerField()
+            other_field = models.IntegerField()
+
+            @cleans_field_with_context('tests.ErrorContextModel.other_field')
+            @cleans_field_with_context('tests.ErrorContextModel.some_field')
+            def clean_some_field(self, some_field, data):
+                if isinstance(some_field, int):
+                    return some_field
+                raise TypeError('some_field is the wrong type')
+
+        dummy = ErrorContextModel(some_field=5, other_field='str')
         with self.assertRaises(TypeError) as ctx:
             pre_save.send(dummy.__class__, instance=dummy)
         self.assertEqual(str(ctx.exception), 'some_field is the wrong type')
